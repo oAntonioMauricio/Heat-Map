d9d72dc616367e668a891c9dce708b2ff8bfdc38
António <oantoniomauricio@gmail.com>
2023-01-17 17:39:46 +0000

    tooltips added

--- scripts.js
+++ scripts.js
@@ -31,17 +31,20 @@ d3.json("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/mas
         .attr("transform", "translate(0," + (h - paddingBottom) + ")")
         .call(xAxis)
 
+    // function to display months
+    function translateMonth(x) {
+        let date = new Date(0, x)
+        let format = d3.utcFormat('%B');
+        return format(date)
+    }
+
     // Y AXIS
     let yScale = d3.scaleBand()
         .domain([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
         .range([0 + padding, h - paddingBottom])
 
     let yAxis = d3.axisLeft(yScale)
-        .tickFormat(x => {
-            let date = new Date(0, x)
-            let format = d3.utcFormat('%B');
-            return format(date)
-        })
+        .tickFormat(x => translateMonth(x))
 
     svg.append("g")
         .attr("id", "y-axis")
@@ -49,7 +52,6 @@ d3.json("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/mas
         .call(yAxis)
 
     // COLOR PALETE FOR TEMP
-
     let tempPalete = ["blue", "#1984c5", "#22a7f0", "#63bff0", "#a7d5ed", "#e2e2e2", "#e1a692", "#de6e56", "#e14b31", "#c23728", "red"];
     let baseTemp = data.baseTemperature
 
@@ -86,6 +88,34 @@ d3.json("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/mas
         }
     }
 
+    // TOOLTIPS
+    tooltip = d3.select("#holder")
+        .append("div")
+        .attr("class", "tooltip")
+        .attr("id", "tooltip")
+        .style("opacity", 0)
+
+    // A function that change this tooltip when the user hover a point.
+    // Its opacity is set to 1: we can now see it.
+    let mouseover = function (d) {
+        tooltip
+            .style("opacity", 1)
+        tooltip
+            .html(`${d.year} ${translateMonth(d.month - 1)}</br> ${tempVariation(baseTemp, d.variance)}ºC</br>${Number.parseFloat(d.variance).toFixed(1)}`)
+            .style("left", (d3.event.pageX + 10) + "px")
+            .style("top", (d3.event.pageY - 40) + "px")
+            .attr("data-year", `${d.year}`)
+        d3.select(this)
+            .style("stroke", "black")
+            .style("stroke-width", 2)
+    }
+
+    let mouseleave = function (d) {
+        tooltip.style("opacity", 0)
+        d3.select(this)
+            .style("stroke", "none")
+    }
+
     // RECTS
     svg.selectAll("rect")
         .data(data.monthlyVariance)
@@ -100,6 +130,8 @@ d3.json("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/mas
         .attr("width", `${w / (2015 - 1753)}`)
         .attr("height", (d) => yScale.bandwidth(d.month - 1))
         .attr("fill", (d) => colorVariation(tempVariation(baseTemp, d.variance)))
+        .on("mouseover", mouseover)
+        .on("mouseleave", mouseleave)
 
     // X AXIS FOR COLORS
     let tempArray = [2.8, 3.9, 5.0, 6.1, 7.2, 8.3, 9.5, 10.6, 11.7, 12.8];
@@ -133,6 +165,6 @@ d3.json("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/mas
         .attr("height", 25)
         .attr("fill", (d) => colorVariation(d))
         .attr("stroke", "black")
-        .attr("stroke-width", 1)  
+        .attr("stroke-width", 1)
 
 })
